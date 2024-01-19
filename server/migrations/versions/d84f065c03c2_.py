60ef4c58f40365fc8de6c935527bcc0ff153d843
Samuel Muli <samuelmuli95@gmail.com>
2024-01-19 13:03:48 +0300

    Run my migrations

---
+++ server/migrations/versions/d84f065c03c2_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d84f065c03c2
+Revises: 
+Create Date: 2024-01-19 12:50:22.888963
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd84f065c03c2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
