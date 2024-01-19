aa87a05b3ad83a41b91ad9962e8aa40f6b136b98
Samuel Muli <samuelmuli95@gmail.com>
2024-01-19 12:47:06 +0300

    Create restaurant_pizzas table

--- server/models.py
+++ server/models.py
@@ -11,6 +11,9 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
 
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurants' , lazy=True)
+
+
     def __repr__(self):
         return f'<Restaurant {self.name} in {self.address}>'
     
@@ -22,7 +25,18 @@ class  Pizza(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False)
     ingredients = db.Column(db.String, nullable=False)
 
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizzas' , lazy=True)
+
     def __repr__(self):
         return f'<Pizza {self.name} with {self.ingredients}>'
-
     
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+
+    def __repr__(self):
+        return f'<Pizza with ID No: {self.id} is {self.price}/=>'
