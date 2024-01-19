4f7c5ab7aa833b116a276a517a472711a84bcd94
Samuel Muli <samuelmuli95@gmail.com>
2024-01-19 12:23:43 +0300

    Create restaurants table

--- server/models.py
+++ server/models.py
@@ -3,3 +3,15 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__='restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Restaurant {self.name} in {self.address}>'
+
+    
