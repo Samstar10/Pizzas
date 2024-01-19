5f0b49433ef6f9a5bf8fa5a3654eb45099c57069
Samuel Muli <samuelmuli95@gmail.com>
2024-01-19 13:33:35 +0300

    Create Get /restaurants path

--- server/app.py
+++ server/app.py
@@ -1,13 +1,13 @@
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 from flask import Flask, make_response, request, jsonify, render_template
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db
+from models import db, Restaurant, Pizza, RestaurantPizza
 
 app = Flask(
     __name__,
@@ -26,7 +26,24 @@ db.init_app(app)
 
 api = Api(app)
 
+class Restaurants(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        restaurants_dict = [
+            {
+                'id': restaurant.id,
+                'name': restaurant.name,
+                'address': restaurant.address
+            } for restaurant in restaurants
+        ]
 
+        return make_response(jsonify(restaurants_dict), 200)
+
+
+
+
+
+api.add_resource(Restaurants, '/restaurants')
 
 if __name__ == '__main__':
     app.run(port=5555)
